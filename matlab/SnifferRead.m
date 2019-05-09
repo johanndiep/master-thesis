6f4b8dc84b606db77434b5f45af053ee82480957
Johann Diep <johanndiep@gmail.com>
2019-05-09 20:48:15 +0200

    working version of SnifferRead.m

--- matlab/SnifferRead.m
+++ matlab/SnifferRead.m
@@ -1,6 +1,6 @@
 % Johann Diep (jdiep@student.ethz.ch) - May 2019
 
-% This program reads and controls the Sniffer module.
+% This program reads and controls the modified Sniffer module.
 
 clear 
 clc
@@ -19,6 +19,7 @@ iterations = 100;
 averaging_number = 50;
 anchors = 6;
 first_iteration = true;
+next_index = false;
 
 %% Range aquisition form each anchor via TWR
 
@@ -31,33 +32,73 @@ for i = 1:anchors
     while index < iterations + 1
         line = fgetl(serial);
        
+        % start with anchor 1 and avoid information overload
         if first_iteration == true
-            while ~strncmpi(line,"Anchor 1",8)
-                line = fgetl(serial);
+            if i == 1
+                while ~strncmpi(line,"Anchor 2",8)
+                    line = fgetl(serial);
+                end
+            elseif i > 1
+                while ~strncmpi(line,"Anchor 1",8)
+                    line = fgetl(serial);
+                end
             end
             first_iteration = false;
         end
         
+        % Abortion statement
+        if line(17) ~= int2str(i)
+            disp("Aborting due to detection of wrong sequence number.");
+            break;
+        end
+        
+        % storing range measurement in array of size (#anchors, #anchors, #measurements)
         if strncmpi(line,"Anchor",6)
             switch line(8)
-                case "1"
-                    range_array(i,1,index) = str2num(line(20:24));
-                case "2"
-                    range_array(i,2,index) = str2num(line(20:24));
-                case "3"
-                    range_array(i,3,index) = str2num(line(20:24));
-                case "4"
-                    range_array(i,4,index) = str2num(line(20:24));
-                case "5"
-                    range_array(i,5,index) = str2num(line(20:24));
-                case "6"
-                    range_array(i,6,index) = str2num(line(20:24));
+                case "1" % anchor 1
+                    range_array(i,1,index) = str2num(line(24:end));
+                    next_index = false;
+                case "2" % anchor 2
+                    range_array(i,2,index) = str2num(line(24:end));
+                    next_index = false;
+                case "3" % anchor 3
+                    range_array(i,3,index) = str2num(line(24:end));
+                    next_index = false;
+                case "4" % anchor 4
+                    range_array(i,4,index) = str2num(line(24:end));
+                    next_index = false;
+                case "5" % anchor 5
+                    range_array(i,5,index) = str2num(line(24:end)); 
+                    
+                    % solves indexing issue at last anchor index
+                    if i == 6 
+                        next_index = true;
+                    elseif i < 6
+                        next_index = false;
+                    end
+                case "6" % anchor 6
+                    range_array(i,6,index) = str2num(line(24:end));
+                    next_index = true;
             end
-        end        
-        index = index + 1;
-    end  
-    index = 0;
-    fwrite(serial, "y");
+        end
+        
+        if next_index % gathered measurement for anchor 1 to 6
+           index = index + 1;
+           next_index = false;
+        end
+    end
     
+    index = 1;
+    fwrite(serial, "y");
+    pause(2);
     first_iteration = true;
 end
+
+%% Averaging measurements
+
+for row = 1:anchors
+   for column = 1:anchors
+      range_mean(row,column) = mean(rmoutliers(permute(range_array(row,column,:),[1,3,2]))); 
+   end
+end
+
